afd143a0d58a532f32d41d8d81dcdb725fedda32
David <David.thiman@gmail.com>
2020-08-26 14:22:42 +0200

    Yes

---
+++ src/Com/MyTraining/Polymorphism/Movie.java
@@ -0,0 +1,15 @@
+package Com.MyTraining.Polymorphism;
+
+public class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+    public String plot(){
+        return "No plot here";
+    }
+
+
+}
+
